8cb82978d195b3c19510618f56e846e3dc7ee173
ersinbulut <ersinblt61@gmail.com>
2022-10-25 00:15:02 +0300

    #31 Traversal Rezervasyon Asp.Net Core 5.0 Mini Proje Custom Identity Validator

---
+++ TraversalCore/Areas/Member/Controllers/CommentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Areas.Member.Controllers
+{
+    public class CommentController : Controller
+    {
+        [Area("Member")]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCore/Models/UserRegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCore.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="LÜTFEN ADINIZI GİRİNİZ")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "LÜTFEN SOYADINIZI GİRİNİZ")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "LÜTFEN KULLANICI ADINIZI GİRİNİZ")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "LÜTFEN MAİL ADRESİNİ GİRİNİZ")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "LÜTFEN ŞİFREYİ GİRİNİZ")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "LÜTFEN ŞİFREYİ TEKRAR GİRİNİZ")]
+        [Compare("Password",ErrorMessage ="ŞİFRELER UYUMLU DEĞİL")]
+        public string ConfirmPassword { get; set; }
+    }
+}
