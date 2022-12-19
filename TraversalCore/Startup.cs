2d7dcd4b662dcc56c784e0411f35ab1a4f70b354
BULUTS <BULUTS@BULUTS>
2022-12-19 17:05:50 +0300

    34 Traversal Rezervasyon Asp.Net Core 5.0 Mini Proje Identity ile Login İşlemi

--- TraversalCore/Startup.cs
+++ TraversalCore/Startup.cs
@@ -72,6 +72,14 @@ namespace TraversalCore
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
